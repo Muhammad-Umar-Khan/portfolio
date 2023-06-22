2b9aba91521fb93acded0085d3b54fb97e3e3ba7
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-06-22 06:14:29 +0500

    font-burtons

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import {BsFillMoonStarsFill} from "react-icons/bs"
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white px-10">
       <section className="min-h-screen">
-        <nav>
-          <h1>Umar Khan</h1>
-          <ul>
-            <li></li>
-            <li className="bg-blue-700 w-20 text-center">
+        <nav className="flex justify-between py-10">
+          <h1 className="text-xl font-burtons">Umar Khan</h1>
+          <ul className="flex items-center">
+            <li className="cursor-pointer text-xl"><BsFillMoonStarsFill /></li>
+            <li className="bg-gradient-to-r from-cyan-500 to-blue-500 py-2 px-4 text-white ml-5 rounded-md">
               <a href="#">RESUME</a>
             </li>
           </ul>
