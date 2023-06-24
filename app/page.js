0f39f9ba6664b15b9bf67d15c7e01c1857ac657a
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-06-24 08:23:16 +0500

    final touches

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillYoutube } from "react-icons/ai";
 import Image from "next/image";
@@ -7,16 +8,21 @@ import fullstack from "../public/fullstack.png";
 import bookstoreApi from "../public/bookstoreApi.png";
 import crypto from "../public/crypto.png";
 import fullstack02 from "../public/fullstack02.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className="bg-white px-10">
-      <section className="min-h-screen">
+    <main className={darkMode ? "dark" : ""}>
+      <section className="min-h-screen px-10 dark:bg-gray-900">
         <nav className="flex justify-between py-10">
-          <h1 className="text-xl font-burtons">Umar Khan</h1>
+          <h1 className="text-xl font-burtons dark:text-white">Umar Khan</h1>
           <ul className="flex items-center">
-            <li className="cursor-pointer text-xl">
-              <BsFillMoonStarsFill />
+            <li
+              className="cursor-pointer text-xl"
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              <BsFillMoonStarsFill className="dark:text-white" />
             </li>
             <li className="bg-gradient-to-r from-cyan-500 to-blue-500 py-2 px-4 text-white ml-5 rounded-md">
               <a href="#">RESUME</a>
@@ -24,13 +30,17 @@ export default function Home() {
           </ul>
         </nav>
         <div className="text-center p-10">
-          <h2 className="text-teal-500 font-Poppins font-bold text-5xl py-2">
+          <h2 className="text-teal-500 font-Poppins font-bold text-5xl py-2 lg:text-6xl">
             Umar Khan
           </h2>
-          <h3 className="text-2xl py-2">Software Engineer</h3>
-          <p className="text-md py-5 leading-8 text-slate-800">
-            I am a software engineer with more than a year of experience working
-            in the industry
+          <h3 className="lg:text-2xl text-xl py-2 dark:text-white">
+            Software Engineer
+          </h3>
+          <p className="font-semibold text-xl py-5 leading-8 text-slate-800 dark:text-white lg:max-w-2xl mx-auto">
+            Hello, Nice to meet you! I am a software engineer with
+            more than a year of experience working in the industry, now i work
+            as independent freelancer dedicated to delivering hight quality
+            softwares. Really looking forward to collabrate on your project
           </p>
         </div>
         <div className="flex justify-center text-5xl text-gray-600 gap-105 py-3">
@@ -40,8 +50,9 @@ export default function Home() {
         <section>
           <h3 className="text-2xl text-teal-500 font-bold my-3">SERVICES</h3>
           <div className="md:flex gap-10">
-            <div className="text-center shadow-lg p-2 rounded-lg">
+            <div className="text-center shadow-lg p-2 rounded-lg dark:bg-white mb-5">
               <Image
+                alt="frontend"
                 src={frontend}
                 width={400}
                 height={200}
@@ -52,8 +63,9 @@ export default function Home() {
                 technologies like react, next, CSS, Tailwind, Bootstrap
               </p>
             </div>
-            <div className="text-center shadow-lg p-2 rounded-lg">
+            <div className="text-center shadow-lg p-2 rounded-lg dark:bg-white mb-5">
               <Image
+                alt="backend"
                 src={backend}
                 width={400}
                 height={200}
@@ -64,8 +76,9 @@ export default function Home() {
                 technologies like react, next, CSS, Tailwind, Bootstrap
               </p>
             </div>
-            <div className="text-center shadow-lg p-2 rounded-lg">
+            <div className="text-center shadow-lg p-2 rounded-lg dark:bg-white mb-5">
               <Image
+                alt="fullstack"
                 src={fullstack}
                 width={400}
                 height={200}
@@ -81,24 +94,20 @@ export default function Home() {
         <section>
           <p className="text-2xl text-teal-500 font-bold my-3">PROJECTS</p>
           <div className="gap-10 md:flex justify-center">
-            <div className="flex flex-col justify-center items-center shadow-lg p-2 rounded-lg">
+            <div className="flex flex-col justify-center items-center shadow-lg p-2 rounded-lg dark:bg-white mb-5 lg:w-4/12">
               <span>
                 <Image
+                  alt="crypto"
                   src={crypto}
                   width={400}
                   height={200}
                   className="mx-auto"
                 />
                 <p className="py-3">
-                  This is an API - backend for an online bookshop. This backend
-                  has two entities, users and books. A user can register and
-                  login. User can read all books and buy a book, while admin-
-                  superuser can add, edit, delete a book. Admin can block or
-                  unblock any user and can see the sales stats of any book for a
-                  specific deuration.
-                  {/* This is frontend project made in in react and tailwindcss.
+                  This is frontend project made in in react and tailwindcss.
                   This is an online payment website just for the frontend and
-                  does not have any backend attached to it */}
+                  does not have any backend attached to it. This is for the
+                  demonstration of my frontend and designing skills
                 </p>
               </span>
               <div className="flex justify-center">
@@ -119,9 +128,10 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div className="text-center shadow-lg p-2 rounded-lg">
+            <div className="text-center shadow-lg p-2 rounded-lg dark:bg-white mb-5 lg:w-4/12">
               <span>
                 <Image
+                  alt="booksapi"
                   src={bookstoreApi}
                   width={400}
                   height={200}
@@ -154,9 +164,10 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div className="text-center shadow-lg p-2 rounded-lg">
+            <div className="text-center shadow-lg p-2 rounded-lg dark:bg-white mb-5 lg:w-4/12">
               <span>
                 <Image
+                  alt="fullstack02"
                   src={fullstack02}
                   width={400}
                   height={200}
